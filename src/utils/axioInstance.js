c695268a7f424dcbb22a3044ae7030a3a616e3b7
himcom31 <hcompany021@gmail.com>
2026-03-01 00:05:53 +0530

    Update BASE_URL to production endpoint

--- src/utils/axioInstance.js
+++ src/utils/axioInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:3000/api"
+const BASE_URL = "https://task-manager-b-1.onrender.com/api"
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
